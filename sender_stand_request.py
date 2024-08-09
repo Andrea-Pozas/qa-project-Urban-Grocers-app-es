ed05293f5ba24cce0d3e69c19a5884280eb67bdc
Andrea Pozas <andrea_pozas_24@outlook.com>
2024-08-08 22:40:51 -0600

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -9,9 +9,6 @@ def post_new_user(body):
                          headers=data.headers)
 
 
-response = post_new_user(data.user_body)
-print(response.status_code)
-
 
 def post_new_client_kit(kit_body, auth_token):
     return requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,
